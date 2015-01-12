f0fb20500b8f41377106f2fd13397d78e744b940
Mark Reid <mreid@mozilla.com>
2015-01-12 10:31:30 -0400

    Implement looping, clean up code.
    Keep checking every 10s if we're in loop mode. Create the S3
    connector only once. Simplify code where possible.

--- cmd/heka-export/main.go
+++ cmd/heka-export/main.go
@@ -30,39 +30,22 @@ import (
 	"github.com/crowdmob/goamz/s3"
 )
 
-type AWSCredentials struct {
-	Key string
-	SecretKey string
-	Region string
-}
-
 type Progress struct {
 	Count int64
 	Bytes int64
 	Errors int32
 }
 
-func makeupload(base string, pattern *regexp.Regexp, creds AWSCredentials, bucket string, bucketPrefix string, dryRun bool, progress *Progress) func(string, os.FileInfo, error) error {
-	//s3Prefix := fmt.Sprintf("s3://%s", bucket)
-	//if bucketPrefix != "" {
-	//	s3Prefix = fmt.Sprintf("%s/%s", s3Prefix, strings.Trim(bucketPrefix, "/"))
-	//}
+// Generate a function to use for uploading files as we walk the file tree from
+// `base`.
+func makeupload(base string, pattern *regexp.Regexp, bucket *s3.Bucket, bucketPrefix string, dryRun bool, progress *Progress) func(string, os.FileInfo, error) error {
+	// Remove any excess path separators from the bucket prefix.
 	bucketPrefix = strings.Trim(bucketPrefix, "/")
 
+	// Get a canonical version of the base dir
 	cleanBase := filepath.Clean(base)
 
-	var s *s3.S3
-	var b *s3.Bucket
-	if !dryRun {
-		auth := aws.Auth{AccessKey: creds.Key, SecretKey: creds.SecretKey}
-		s = s3.New(auth, aws.Regions[creds.Region])
-		b = s.Bucket(bucket)
-	} else {
-		// s declared and not used :(
-		_ = s
-		_ = b
-	}
-
+	// Create a closure for the upload function.
 	return func(path string, fi os.FileInfo, err error) (errOut error) {
 		if err != nil {
 			return err
@@ -80,40 +63,53 @@ func makeupload(base string, pattern *regexp.Regexp, creds AWSCredentials, bucke
 		}
 
 		// Make sure we're comparing apples to apples when stripping off the
-		// base path.
+		// base path. Use the canonical versions of both.
 		cleanPath := filepath.Clean(path)
-		s3Path := fmt.Sprintf("%s/%s", bucketPrefix, cleanPath[len(cleanBase)+1:])
+		relPath := cleanPath[len(cleanBase)+1:]
+
+		// If we encounter a file
+		//    /path/to/base/foo/bar/baz
+		//    <--- base ---><-- rel -->
+		// and our bucket prefix is `hello/files`, our file in S3 will be at
+		//    s3://bucket-name/hello/files/foo/bar/baz
+		//                     <- prefix -><-- rel -->
+		s3Path := fmt.Sprintf("%s/%s", bucketPrefix, relPath)
+
+		// Update progress Count whether we were successful or not.
+		progress.Count += 1
 
 		if dryRun {
 			fmt.Printf("Dry run. Not uploading item to %s\n", s3Path)
-		} else {
-			fmt.Printf("Uploading item to: %s\n", s3Path)
-			reader, err := os.Open(path)
-			if err != nil {
-				fmt.Printf("Error opening %s for reading: %s\n", path, err)
-				errOut = err
-				progress.Errors++
-			} else {
-				err := b.PutReader(s3Path, reader, fi.Size(), "binary/octet-stream", s3.BucketOwnerFull, s3.Options{})
-				if err != nil {
-					errOut = err
-					progress.Errors++
-				} else {
-					err := os.Remove(path)
-					if err != nil {
-						fmt.Printf("Error removing %s: %s\n", path, err)
-						errOut = err
-						progress.Errors++
-					}
-				}
-			}
-
+			return
 		}
 
-		progress.Count += 1
+		fmt.Printf("Uploading item to: %s\n", s3Path)
+		reader, err := os.Open(path)
+		if err != nil {
+			fmt.Printf("Error opening %s for reading: %s\n", path, err)
+			progress.Errors++
+			return err
+		}
+
+		err = bucket.PutReader(s3Path, reader, fi.Size(), "binary/octet-stream", s3.BucketOwnerFull, s3.Options{})
+		if err != nil {
+			progress.Errors++
+			return err
+		}
+
+		// Count the bytes for this file as progress if there were
+		// no upoad errors.
 		progress.Bytes += fi.Size()
 
-		return errOut
+		// Now remove the file locally.
+		err = os.Remove(path)
+		if err != nil {
+			fmt.Printf("Error removing %s: %s\n", path, err)
+			progress.Errors++
+			errOut = err
+		}
+
+		return
 	}
 }
 
@@ -155,20 +151,48 @@ func main() {
 	var rate float64
 	var uploadMB float64
 
-	startTime := time.Now().UTC()
-
-	credentials := AWSCredentials{*flagAWSKey, *flagAWSSecretKey, *flagAWSRegion}
-	err = filepath.Walk(*flagBase, makeupload(*flagBase, pattern, credentials, *flagBucket, *flagBucketPrefix, *flagDryRun, &progress))
-	if err != nil {
-		fmt.Printf("Error reading files from %s: %s\n", *flagBase, err)
-		os.Exit(4)
+	var b *s3.Bucket
+	if !*flagDryRun {
+		auth := aws.Auth{AccessKey: *flagAWSKey, SecretKey: *flagAWSSecretKey}
+		s := s3.New(auth, aws.Regions[*flagAWSRegion])
+		b = s.Bucket(*flagBucket)
+	} else {
+		// b declared and not used :(
+		_ = b
 	}
 
-	uploadMB = float64(progress.Bytes) / 1024.0 / 1024.0
-	duration := time.Now().UTC().Sub(startTime).Seconds()
+	for true {
+		progress = Progress{}
+		startTime := time.Now().UTC()
+		err = filepath.Walk(*flagBase, makeupload(*flagBase, pattern, b, *flagBucketPrefix, *flagDryRun, &progress))
+		if err != nil {
+			fmt.Printf("Error reading files from %s: %s\n", *flagBase, err)
+		}
 
-	if duration > 0 {
-		rate = uploadMB / duration
+		if progress.Count > 0 {
+			uploadMB = float64(progress.Bytes) / 1024.0 / 1024.0
+			duration := time.Now().UTC().Sub(startTime).Seconds()
+
+			if duration > 0 {
+				rate = uploadMB / duration
+			} else {
+				rate = 0
+			}
+			fmt.Printf("Uploaded %d files containing %.2fMB in %.02fs (%.02fMB/s). Encountered %d errors.\n", progress.Count, uploadMB, duration, rate, progress.Errors)
+		} else {
+			// We didn't upload any files.
+			if !*flagLoop {
+				fmt.Println("Nothing to upload")
+			} else {
+				// Only sleep if we didn't find anything to upload. If we did upload
+				// something, we want to try again right away.
+				fmt.Println("Waiting for files to upload...")
+				time.Sleep(10 * time.Second)
+			}
+		}
+
+		if !*flagLoop {
+			break
+		}
 	}
-	fmt.Printf("Uploaded %d files containing %.2fMB in %.02fs (%.02fMB/s). Encountered %d errors.\n", progress.Count, uploadMB, duration, rate, progress.Errors)
 }
